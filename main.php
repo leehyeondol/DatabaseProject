9772c50f3bd3b2d536937a31c85c26adab4013ff
hyeondo <guseh5634@gmail.com>
2023-11-29 21:04:19 +0900

    customer, fashionlist, product까지

--- main.php
+++ main.php
@@ -1,13 +1,14 @@
 <?php
 // 데이터베이스 연결 정보
 $servername = "192.168.56.101";
-$port=4567;
+$port = 4567;
 $username = "guseh5634";
 $password = "gmlakd5634!";
 $dbname = "databasePJ";
 
 // 데이터베이스 연결
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
+
 // 연결 확인
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -31,28 +32,71 @@ $result_fashion = $conn->query($sql_fashion);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>메인페이지</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+        }
+
+        .header {
+            background-color: #333;
+            color: #fff;
+            padding: 15px;
+            text-align: right;
+        }
+
+        .logout-btn {
+            background-color: #4CAF50;
+            color: #fff;
+            padding: 10px 20px;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+
+        .logout-btn:hover {
+            background-color: #45a049;
+        }
+
+        .fashion-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+            padding: 20px;
+        }
+
+        .fashion-item {
+            margin: 10px;
+            text-align: center;
+            width: 400px; /* 설정한 폭에 따라 다음 줄로 넘어감 */
+        }
+    </style>
 </head>
 <body>
+    <div class="header">
+        <a class="logout-btn" href="logout.php">로그아웃</a>
+    </div>
+
     <h2>패션 목록</h2>
 
-    <?php
-    // 패션 목록 출력
-    if ($result_fashion->num_rows > 0) {
-        while ($row_fashion = $result_fashion->fetch_assoc()) {
-            echo "<div>";
-            echo "<a href='view_fashion.php?fashion_id=" . $row_fashion['fashion_id'] . "'>";
-            echo "<img src='" . $row_fashion['fs_picture'] . "' alt='Fashion Image'>";
-            echo "</a>";
-            echo "<p>패션 ID: " . $row_fashion['fashion_id'] . "</p>";
-            echo "<p>패션 정보: " . $row_fashion['fs_info'] . "</p>";
-            echo "</div>";
+    <div class="fashion-container">
+        <?php
+        // 패션 목록 출력
+        if ($result_fashion->num_rows > 0) {
+            while ($row_fashion = $result_fashion->fetch_assoc()) {
+                echo "<div class='fashion-item'>";
+                echo "<a href='view_fashion.php?fashion_id=" . $row_fashion['fashion_id'] . "'>";
+                echo "<img src='" . $row_fashion['fs_picture'] . "' alt='Fashion Image'>";
+                echo "</a>";
+                echo "<p>패션 ID: " . $row_fashion['fashion_id'] . "</p>";
+                echo "<p>패션 정보: " . $row_fashion['fs_info'] . "</p>";
+                echo "</div>";
+            }
+        } else {
+            echo "등록된 패션이 없습니다.";
         }
-    } else {
-        echo "등록된 패션이 없습니다.";
-    }
-    ?>
-
-    <!-- 추가적인 디자인 및 레이아웃을 원하시면 여기에 추가하시면 됩니다. -->
+        ?>
+    </div>
 
     <!-- 데이터베이스 연결 종료 -->
     <?php
--- view_fashion.php
+++ view_fashion.php
@@ -1,7 +1,7 @@
 <?php
 // 데이터베이스 연결 정보
 $servername = "192.168.56.101";
-$port=4567;
+$port = 4567;
 $username = "guseh5634";
 $password = "gmlakd5634!";
 $dbname = "databasePJ";
@@ -24,60 +24,42 @@ if (!isset($_SESSION['username'])) {
 // fashion_id를 받아옴
 $fashion_id = $_GET['fashion_id'];
 
-// 해당 fashion_id에 대한 PRODUCT 정보 조회 쿼리
-$sql_product = "SELECT * FROM FashionList WHERE fashion_id = $fashion_id";
-$result_product = $conn->query($sql_product);
+// 해당 fashion_id에 대한 PRODUCTS 정보 조회 쿼리
+$sql_products = "SELECT Products.* FROM Products
+                JOIN FashionList_Products ON Products.product_id = FashionList_Products.product_id
+                WHERE FashionList_Products.fashion_id = $fashion_id";
+$result_products = $conn->query($sql_products);
 
 // 결과가 있는지 확인
-if ($result_product->num_rows > 0) {
-    $row_product = $result_product->fetch_assoc();
-    $product_id = $row_product['product_id'];
+if ($result_products->num_rows > 0) {
+    echo "<h2>Fashion 상세 정보</h2>";
+    echo "<img src='' alt='Fashion Image'>";
+    echo "<br>";
+    echo "<br>";
+    echo "<hr>";
+    echo "<hr>";
+    echo "<br>";
+    echo "<br>";
 
-    // PRODUCT 정보 조회 쿼리
-    $sql_product_info = "SELECT * FROM Products WHERE product_id = $product_id";
-    $result_product_info = $conn->query($sql_product_info);
-
-    // 결과가 있는지 확인
-    if ($result_product_info->num_rows > 0) {
-        $row_product_info = $result_product_info->fetch_assoc();
-    } else {
-        echo "해당 상품 정보가 없습니다.";
+    // 각각의 PRODUCT 정보 출력
+    echo "<div class='product-container'>";
+    while ($row_product = $result_products->fetch_assoc()) {
+        echo "<img src='" . $row_product['p_picture'] . "' alt='Product Image'>";
+        echo "<div class='product-item'>";
+        echo "<p>상품 ID: " . $row_product['product_id'] . "</p>";
+        echo "<p>상품명: " . $row_product['product_name'] . "</p>";
+        echo "<p>판매 사이트: " . $row_product['sell_site'] . "</p>";
+        echo "<p>상품 정보: " . $row_product['p_info'] . "</p>";
+        echo "<p>가격: " . $row_product['price'] . "</p>";
+        echo "</div>";
+        echo "<br>";
+        echo "<br>";
     }
+    echo "</div>";
 } else {
-    echo "해당 패션 정보가 없습니다.";
+    echo "해당 패션 정보에 대한 상품이 없습니다.";
 }
 
+// 데이터베이스 연결 종료
+$conn->close();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fashion 상세 정보</title>
-</head>
-<body>
-    <h2>Fashion 상세 정보</h2>
-
-    <?php
-    // PRODUCT 정보 출력
-    if (isset($row_product_info)) {
-        echo "<p>상품 ID: " . $row_product_info['product_id'] . "</p>";
-        echo "<p>상품명: " . $row_product_info['product_name'] . "</p>";
-        echo "<p>판매 사이트: " . $row_product_info['sell_site'] . "</p>";
-        echo "<p>상품 정보: " . $row_product_info['p_info'] . "</p>";
-        echo "<img src='" . $row_product_info['p_picture'] . "' alt='Product Image'>";
-        echo "<p>가격: " . $row_product_info['price'] . "</p>";
-    } else {
-        echo "상세 정보가 없습니다.";
-    }
-    ?>
-
-    <!-- 추가적인 디자인 및 레이아웃을 원하시면 여기에 추가하시면 됩니다. -->
-
-    <!-- 데이터베이스 연결 종료 -->
-    <?php
-    $conn->close();
-    ?>
-</body>
-</html>
